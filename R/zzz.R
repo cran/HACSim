82ac2b98c6a77a96ba9513d28ea4e422d5900123
Jarrett D. Phillips <phillipsjarrett1@gmail.com>
2020-01-07 20:50:05 +0000

    version 1.0.5

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,11 @@
-envr <- NULL 
+envr <- NULL
 
 .onLoad <- function(...) {
-  envr <<- new.env()  # when package is loaded, create new environment to store needed variables 
+  envr <<- new.env() # when package is loaded, create new environment to store needed variables
 }
 
 .onAttach <- function(...) {
-  packageStartupMessage("This is HACSim 1.0.4 \n
+  packageStartupMessage("This is HACSim 1.0.5 \n
 Type ?HACHypothetical to see how to set up objects to run \n simulations of haplotype accumulation for hypothetical species \n 
 Type ?HACReal to see how to set up objects to run \n simulations of haplotype accumulation for real species \n 
 Type ?HAC.simrep to see how to run simulations of haplotype \n accumulation curves")
