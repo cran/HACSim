35d420a3a556109ec35e99cfa352cffa30114442
Jarrett D. Phillips <phillipsjarrett1@gmail.com>
2022-06-13 05:50:16 +0000

    version 1.0.6-1

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,6 @@
-### Functions needed when loding HACSim ###
+### Functions needed when loading HACSim ###
 
 envr <- NULL
-
 
 .onLoad <- function(...) {
   envr <<- new.env() # when package is loaded, create new environment to store needed variables
